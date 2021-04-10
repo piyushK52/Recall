bcbfc10a98ec576b5d0ba031fd2bcff770b2c5f4
piyushAmplus <piyush.kumar@amplussolar.com>
2021-04-10 23:37:09 +0530

    recall details

--- lib/screens/recall_details.dart
+++ lib/screens/recall_details.dart
@@ -63,17 +63,18 @@ class _RecallDetailsState extends State<RecallDetails> {
                       ),
                       Container(
                         constraints: BoxConstraints(
-                          maxWidth: 250,
+                          maxWidth: 220,
                         ),
                         child: Text(
                           'Some random text that is too long',
                           style: CustomAppTheme.heading1.copyWith(
-                            fontSize: 25,
+                            fontSize: 22,
                           ),
                           maxLines: 1,
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
+                      Spacer(),
                       Container(
                         alignment: Alignment.center,
                         height: 25,
@@ -94,9 +95,139 @@ class _RecallDetailsState extends State<RecallDetails> {
                           ),
                         ),
                       ),
+                      Spacer(),
                     ],
                   ),
-                )
+                ),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        left: 30,
+                        right: 30,
+                        top: 10,
+                        bottom: 10,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Upcoming Session",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(
+                                  top: 10,
+                                ),
+                                child: Text(
+                                  "28 July 2020 at 3:00 PM",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: CustomAppTheme.primaryColor
+                                      .withOpacity(0.7),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 12,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        left: 30,
+                        right: 30,
+                        top: 10,
+                        bottom: 10,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Description",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                              top: 10,
+                            ),
+                            child: Text(
+                              "Some random event text which should be of maximum 50 characters or something like that, will try to keep this short",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.grey,
+                              ),
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    left: 30,
+                    right: 30,
+                    bottom: 10,
+                  ),
+                  child: Text(
+                    "Files",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
