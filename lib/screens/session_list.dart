5fcaeab446d9953eb9faadba6710e0ea16f652db
piyushAmplus <piyush.kumar@amplussolar.com>
2021-04-11 19:59:10 +0530

    progress on session list UI

--- lib/screens/session_list.dart
+++ lib/screens/session_list.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:recall/values/custom_app_theme.dart';
 import 'package:recall/widgets/header.dart';
+import 'package:intl/intl.dart';
 
 class SessionList extends StatelessWidget {
   static const routeName = './main-screen/recall-details/session-list';
+
+  final double cirleDia = 50;
+  final activeColor = CustomAppTheme.primaryColor;
+  final deactiveColor = Colors.grey;
 
   List<DateTime> sessions = [];
   int completed;
@@ -26,6 +32,9 @@ class SessionList extends StatelessWidget {
                   Navigator.of(context).pop("random");
                 },
               ),
+              SizedBox(
+                height: 20,
+              ),
               Expanded(
                 child: ListView.builder(
                   itemCount: sessions.length,
@@ -43,8 +52,64 @@ class SessionList extends StatelessWidget {
 
   _sessionItem(index) {
     return Container(
-      height: 50,
+      // height: 50,
+      padding: EdgeInsets.only(
+        left: 20,
+        right: 20,
+      ),
       color: Colors.lightBlueAccent,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Stack(
+              children: [
+                Container(
+                  height: cirleDia,
+                  width: cirleDia,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(cirleDia / 2),
+                    color: index + 1 > completed ? deactiveColor : activeColor,
+                  ),
+                  child: Text(
+                    (index + 1).toString(),
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                index != sessions.length - 1
+                    ? Container(
+                        height: 60,
+                        width: 10,
+                        margin: EdgeInsets.only(
+                          top: 30,
+                          left: 20,
+                          bottom: 3,
+                        ),
+                        decoration: BoxDecoration(
+                          color: index + 1 > completed
+                              ? deactiveColor
+                              : activeColor,
+                        ),
+                      )
+                    : Container(),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              top: cirleDia / 2 - 10,
+              left: 20,
+            ),
+            child: Text(
+              DateFormat('dd MMM yyyy   hh:mm a').format(sessions[index]),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
